6d99dfbe19e6123876eae4a2306084f875583fca
ahiazd <ahiazd@gmail.com>
2022-04-06 22:17:14 +0300

    Create pythonApp.py

---
+++ pythonApp.py
@@ -0,0 +1,11 @@
+
+import time
+import os
+import subprocess
+
+print (" ")
+print (" ~~~~~~~ W E L C O M E   TO  MY  APPLICATION ~~~~~~~ ")
+print (" ")
+print (" Loading top stats .....")
+time.sleep(2)
+subprocess.run(["top"])
